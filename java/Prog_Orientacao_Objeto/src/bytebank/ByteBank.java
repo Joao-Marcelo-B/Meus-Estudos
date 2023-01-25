ac46aac3892b8a75a0c94b58d1cd388e3406df4b
Joao-Marcelo-Not <joaomarcelobatistanarciso@gmail.com>
2023-01-25 17:36:40 -0300

    Adicionando novas aulas de Java POO

---
+++ java/Prog_Orientacao_Objeto/src/bytebank/ByteBank.java
@@ -0,0 +1,10 @@
+package bytebank;
+
+public class ByteBank {
+	
+	public static void main(String[] args) {
+		ContaCorrente c1 = new ContaCorrente();
+		c1.abrirConta("1010-X", "CP", "João Marcelo", true);
+		System.out.println(c1.getSaldo());
+	}
+}
---
+++ java/Prog_Orientacao_Objeto/src/bytebank/ContaCorrente.java
@@ -0,0 +1,79 @@
+package bytebank;
+
+public class ContaCorrente {
+	public String numConta;
+	protected String tipo;   //CC - Conta Corrente //CP - Conta Poupança
+	private String titular;
+	private double saldo;
+	private boolean status;
+	
+	public void abrirConta(String numConta, String tipo, String titular, boolean status) {
+		this.numConta = numConta;
+		this.tipo = tipo;
+		this.titular = titular;
+		this.status = status;
+		 
+		if(this.tipo == "CC" ) {
+			this.saldo = 50;
+		} else {
+			if(this.tipo == "CP") {
+				this.saldo = 150;
+			}
+		 }
+	}
+	
+	public String getNumConta() {
+		return numConta;
+	}
+	public void setNumConta(String numConta) {
+		this.numConta = numConta;
+	}
+	
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	public String getTitular() {
+		return titular;
+	}
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+	
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	public boolean getStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	
+	public void getInformacoes() {
+		System.out.println(this.numConta);
+		System.out.println(this.tipo);
+		
+		System.out.println(this.titular);
+		System.out.println(this.saldo);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
