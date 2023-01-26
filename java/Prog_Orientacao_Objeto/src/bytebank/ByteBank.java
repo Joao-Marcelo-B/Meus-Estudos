6f93ebeda7ef631225b3163c7c2636d54c45080f
Joao-Marcelo-Not <joaomarcelobatistanarciso@gmail.com>
2023-01-26 10:03:57 -0300

    Adicionando novas aula de Java Orientada a Objeto

--- java/Prog_Orientacao_Objeto/src/bytebank/ByteBank.java
+++ java/Prog_Orientacao_Objeto/src/bytebank/ByteBank.java
@@ -3,8 +3,13 @@ package bytebank;
 public class ByteBank {
 	
 	public static void main(String[] args) {
+		
 		ContaCorrente c1 = new ContaCorrente();
-		c1.abrirConta("1010-X", "CP", "João Marcelo", true);
-
+		c1.abrirConta(1010, "CC", "Joao Marcelo");
+		c1.estadoAtual();
+		
+		ContaCorrente c2 = new ContaCorrente();
+		c2.abrirConta(1011, "CP", "Maria");
+		c2.estadoAtual();
 	}
 }
--- java/Prog_Orientacao_Objeto/src/bytebank/ContaCorrente.java
+++ java/Prog_Orientacao_Objeto/src/bytebank/ContaCorrente.java
@@ -1,35 +1,58 @@
 package bytebank;
 
 public class ContaCorrente {
-	public String numConta;
+	public int numConta;
 	protected String tipo;   //CC - Conta Corrente //CP - Conta Poupança
 	private String titular;
 	private double saldo;
 	private boolean status;
 	
-	public void abrirConta(String numConta, String tipo, String titular, boolean status) {
-		this.numConta = numConta;
-		this.tipo = tipo;
-		this.titular = titular;
-		this.status = status;
-		 
-		if(this.tipo == "CC" ) {
-			this.saldo = 50;
-		} else {
-			if(this.tipo == "CP") {
-				this.saldo = 150;
-			}
-		 }
+	public void estadoAtual() {
+		System.out.println("------------------------------");
+		System.out.println("Conta: " + this.getNumConta());
+		System.out.println("Tipo: " + this.getTipo());
+		System.out.println("Titular: " + this.getTitular());
+		System.out.println("Saldo: " + this.getSaldo());
+		System.out.println("Status: " + this.getStatus());
+		System.out.println("------------------------------");
 	}
 	
-	/*public void fecharConta() {
-		
-	}*/
+	public ContaCorrente() {
+		this.setSaldo(0);
+		this.setStatus(false);
+	}
+	
+	public void abrirConta(int numConta, String tipo, String titular) {
+		this.setNumConta(numConta);
+		this.setTipo(tipo);
+		this.setTitular(titular);
+		this.setStatus(true);
+		 
+		if(this.tipo == "CC") {
+			this.setSaldo(50);
+		} else if(this.tipo == "CP") {
+				this.setSaldo(150);
+			}
+		System.out.println("Conta aberta com sucesso.");
+	}
+	
+	public void fecharConta() {
+		if(this.saldo > 0) {
+			
+			System.out.println("Para fechar a conta você deve sacar todo seu saldo.");
+		} else if(this.saldo < 0) {
+				System.out.println("Para fechar a conta você deve depositar o saldo devedor.");
+			} else {
+				this.status = false;
+				System.out.println("Sua conta foi fechada com sucesso.");
+			}
+	}
 	
 	public void depositar(double valorDeDeposito) {
 		
-		if(this.status) {
-			this.saldo += valorDeDeposito;
+		if(this.getStatus()) {
+			setSaldo(getSaldo() + valorDeDeposito);
+			System.out.println("Deposito realicado na conta: " + this.getNumConta());
 		} else {
 			System.out.println("Essa conta não existe ou está inátiva.");
 		}
@@ -37,17 +60,43 @@ public class ContaCorrente {
 	
 	public void sacar(double valorDeSaque) {
 		
-		if(this.status && this.saldo > 0 && (this.saldo - valorDeSaque) >= 0) {
-			this.saldo -= valorDeSaque;
+		if(this.getStatus()) {
+			if(this.getSaldo() >= valorDeSaque) {
+				this.setSaldo(this.getSaldo() - valorDeSaque);
+				System.out.println("Saque realizado na conta de: " + this.getNumConta());
+			} else {
+				System.out.println("Saldo Insuficiente.");
+			}
+			
 		} else {
-			System.out.println("Nao foi possível fazer o saque.");
+			System.out.println("A conta está inátiva.");
 		}
 	}
 	
-	public String getNumConta() {
+	public void pagarMensal() {
+		float valor = 0;
+		if(this.tipo == "CC") {
+			valor = 12;
+		} else {
+			if(this.tipo == "CP") {
+				valor = 20;
+			}
+		}
+		
+		if(this.getStatus()) {
+			if(this.getSaldo() > valor) {
+				this.setSaldo(this.getSaldo() - valor);
+				System.out.println("Mensalidade paga com sucesso.");
+			} else {
+				System.out.println("Impossivel pagar.");
+			}
+		}
+	}
+	
+	public int getNumConta() {
 		return numConta;
 	}
-	public void setNumConta(String numConta) {
+	public void setNumConta(int numConta) {
 		this.numConta = numConta;
 	}
 	
@@ -82,7 +131,6 @@ public class ContaCorrente {
 	public void getInformacoes() {
 		System.out.println(this.numConta);
 		System.out.println(this.tipo);
-		
 		System.out.println(this.titular);
 		System.out.println(this.saldo);
 	}
