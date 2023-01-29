5d060e84f79bc967b483298e77b3ceebe297c5c3
Joao-Marcelo-Not <joaomarcelobatistanarciso@gmail.com>
2023-01-29 11:29:11 -0300

    Adicionando novas aulas de Java POO

--- java/Prog_Orientacao_Objeto/src/uec/Luta.java
+++ java/Prog_Orientacao_Objeto/src/uec/Luta.java
@@ -13,4 +13,30 @@ public class Luta {
 	public void lutar() {
 		
 	}
+	
+	public String getDesafiado() {
+		return desafiado;
+	}
+	public void setDesafiado(String desafiado) {
+		this.desafiado = desafiado;
+	}
+	public String getDesafiante() {
+		return desafiante;
+	}
+	public void setDesafiante(String desafiante) {
+		this.desafiante = desafiante;
+	}
+	public int getRounds() {
+		return rounds;
+	}
+	public void setRounds(int rounds) {
+		this.rounds = rounds;
+	}
+	public int getAprovada() {
+		return aprovada;
+	}
+	public void setAprovada(int aprovada) {
+		this.aprovada = aprovada;
+	}
+	
 }
